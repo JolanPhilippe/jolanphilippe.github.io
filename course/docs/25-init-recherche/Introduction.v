1d7d7f38ebaf4202880f0adc4eb10a2d732ca947
Jolan Philippe <jolan.philippe1@univ-orleans.fr>
2025-11-24 09:30:40 +0100

    Content for Init recherche

---
+++ course/docs/25-init-recherche/Introduction.v
@@ -0,0 +1,47 @@
+(* From: HPCS 2017 - Tutorial                          *)
+(* http://frederic.loulergue.eu/hpcs2017               *)
+(* Frederic Loulergue                                  *)
+(*   School of Informatics Computing and Cyber Systems *)
+(*   Northern Arizona University                       *)
+
+Section Examples.
+
+  Parameters A B C : Prop.
+
+  Lemma proof1:
+    A->B->(A->C)->(B->C)->C.
+  Proof.
+    intro HA.
+    intro HB.
+    intro HAC.
+    Show Proof.
+    intro HBC.
+    apply HAC.
+    assumption.
+  Qed.
+
+  Lemma proof2:
+    A->B->(A->C)->(B->C)->C.
+  Proof.
+    intros HA HB HAC HBC.
+    apply HBC.
+    assumption.
+  Qed.
+  
+  Print proof1.
+  
+  Print proof2.
+  
+  Definition proof3:
+    forall (A B C:Prop),A->B->(A->C)->(B->C)->C :=
+    fun A B C HA HB HAC HBC => (HAC HA).
+  
+  Definition proof4:
+    forall (A B C:Prop),A->B->(A->C)->(B->C)->C.
+  Proof.
+    auto.
+  Qed.
+
+End Examples.
+
+
